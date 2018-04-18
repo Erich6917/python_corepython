90269a9cb335208e6af53628cb077d17bb4be0f1
Nigel6917 <1065120559@qq.com>
2018-04-18 15:41:14 +0800

    UTIL -打印工具类更新

--- util/loggerUtil.py
+++ util/loggerUtil.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2017/12/21 
-# @Author  : LIYUAN134
+# @Author  : ErichLee ErichLee@qq.com
 # @File    : loggerUtil.py
 # @Commment: 
 #            
@@ -17,6 +17,22 @@ def infos(*args):
             print each,
         print
         logging.info(args)
+
+
+def ljinfos(lmgs, *args):
+    """
+    :param lmgs: 打印头 需要格式化长度
+    :param args: 变量参数依次打印
+    """
+    if lmgs:
+        print '[ ' + lmgs.ljust(30) + ' ]:',
+    else:
+        print '[ ' + "".ljust(30) + ' ]:',
+
+    if any(args):
+        for each in args:
+            print each,
+    print
 
 
 def printLine():
